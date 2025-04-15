6b21a0d1d483c87b16bacce2227415b99ae95d49
Lucas <lucas8camargo@protonmail.com>
2025-04-15 15:51:49 -0300

    Primeiro commit - CRUD de Produtos com ASP.NET Core MVC com Razor Pages

---
+++ ProdutosCRUD/ProdutosCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProdutosCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ProdutosCRUD/ProdutosCRUD/Models/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProdutosCRUD.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O preço é obrigatório")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
+        public decimal Preco { get; set; }
+
+        [StringLength(500, ErrorMessage = "A descrição deve ter no máximo 500 caracteres")]
+        public string Descricao { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A quantidade em estoque é obrigatória")]
+        [Range(0, int.MaxValue, ErrorMessage = "A quantidade não pode ser negativa")]
+        public int Estoque { get; set; }
+    }
+}
